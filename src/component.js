34b351c71afc16988bdf647523e6b0f43cb5d81b
Pier <p.delaunay1@gmail.com>
2024-06-03 18:13:11 +0400

    maj code starter

--- src/component.js
+++ src/component.js
@@ -1,4 +1,4 @@
-export default (text = "Hello, Webpack !") => {
+export default (text = "Hello, Webpack ! !!!! ") => {
   const element = document.createElement("h1");
 
   element.innerHTML = text;
